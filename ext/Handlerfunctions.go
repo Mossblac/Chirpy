2f91235488c2ae5359a0606ffd52c067f8100efd
Mossblac <Murphyrmurphy2000@gmail.com>
2025-08-13 17:43:26 -0600

    added some notes and print outputs for testing

--- ext/Handlerfunctions.go
+++ ext/Handlerfunctions.go
@@ -13,18 +13,26 @@ type ApiConfig struct {
 func (cfg *ApiConfig) MetricsINC(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.FileserverHits.Add(1)
+		fmt.Printf("hit added\n")
 		next.ServeHTTP(w, r)
 	})
 
 }
 
+/* the middleware function above, returns a handler function like those below, it follows the
+signature of the Handler interface and therefore returns a Handler.
+the "next" input handler is the handler that the middleware wraps around.
+see the use case in main()*/
+
 func (cfg *ApiConfig) ShowCountHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.FileserverHits.Load())))
+	fmt.Printf("Total Hits: %v\n", cfg.FileserverHits.Load())
 }
 
 func (cfg *ApiConfig) ResetCountHandler(w http.ResponseWriter, r *http.Request) {
 	cfg.FileserverHits.Store(0)
 	w.Write([]byte(fmt.Sprintln("hit count reset")))
+	fmt.Printf("hit counter reset\n")
 }
 
 func HealthzHandler(w http.ResponseWriter, r *http.Request) {
