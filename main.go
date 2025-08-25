108d2fa8b3b58f156258bf3d84c4656f31c0c535
Mossblac <Murphyrmurphy2000@gmail.com>
2025-08-24 20:31:19 -0600

    just getting rid of my notes

--- main.go
+++ main.go
@@ -58,17 +58,6 @@ func main() {
 
 	mux.Handle("/app/", cfg.MetricsINC(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 
-	/*
-		MetricsINC returns a handlerfunc that matches the Handler interface. it runs the code within that function just like
-		ResetCounterHandler and ShowCountHandler, but then runs the handler that is set as its input
-
-		so here it provides a function that adds to the Hitcounter, and then after it has done that returns the handler
-		that is set as the input, which in this case is the fileserver handler
-
-		so instead of a handler function assigned to its own page, this function "wraps" around
-		the function for the main page: "/app/"
-	*/
-
 	err = http.ListenAndServe(":8080", mux)
 	fmt.Printf("%v", err)
 
